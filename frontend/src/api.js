ee8f8e66146a8379cd60d2b4394b070e9dc099f9
Ramadhani Ramadhani <ramadhaniramadhani0311@gmail.com>
2024-12-26 23:46:01 +0300

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,1 +1,13 @@
+import axios from 'axios';
 
+const API_URL = 'http://localhost:5000/api';
+
+export const translateText = async (text, targetLang) => {
+  const response = await axios.post(`${API_URL}/translate`, { text, targetLang });
+  return response.data;
+};
+
+export const searchWord = async (word) => {
+  const response = await axios.get(`${API_URL}/dictionary/${word}`);
+  return response.data;
+};
