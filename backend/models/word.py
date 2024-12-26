0c816933ec90f1d16a956cbd9f3f718f768e7cdc
Ramadhani Ramadhani <ramadhaniramadhani0311@gmail.com>
2024-12-26 23:12:41 +0300

    Update word.py

--- backend/models/word.py
+++ backend/models/word.py
@@ -1,1 +1,8 @@
-code
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client['swahili-app']
+words_collection = db['words']
+
+def find_word(word):
+    return words_collection.find_one({"word": word})
