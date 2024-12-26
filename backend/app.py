fcdc013a953c229925b8334807bd1816ea9ae91d
Ramadhani Ramadhani <ramadhaniramadhani0311@gmail.com>
2024-12-26 23:45:06 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,16 @@
+from flask import Flask
+from flask_cors import CORS
+from .routes.translate import translate_blueprint
+from .routes.dictionary import dictionary_blueprint
+from dotenv import load_dotenv
 
+load_dotenv()
+
+app = Flask(__name__)
+CORS(app)
+
+app.register_blueprint(translate_blueprint, url_prefix='/api')
+app.register_blueprint(dictionary_blueprint, url_prefix='/api')
+
+if __name__ == '__main__':
+    app.run(port=5000)
