bd72b67e41198cb22274ea85e2191ccad1dd9f96
Ramadhani Ramadhani <ramadhaniramadhani0311@gmail.com>
2024-12-26 23:47:32 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import Translator from './components/Translator';
 
+function App() {
+  return (
+    <div>
+      <h1>Swahili Learning Platform</h1>
+      <Translator />
+    </div>
+  );
+}
+
+export default App;
